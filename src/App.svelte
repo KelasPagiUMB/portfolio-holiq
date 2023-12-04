62bd86b6263619d0c13954766e7612cb35c0bc66
holiq <holiq.ibrahim376@gmail.com>
2023-12-04 21:53:31 +0700

    feat: setup portfolio

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  //
+</script>
+
+<header>
+  <nav class="navbar">
+    <h1 class="logo">holiq</h1>
+    <ul class="nav-links">
+      <li><a class="active" href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <h1>HOLIQ IBRAHIM</h1>
+</main>
+
